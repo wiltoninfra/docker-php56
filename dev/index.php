fef400b7bfd3df2f124cc9b9ce35a326da824017
Wilton Guilherme <wilton.guilherme@tbfconsultoria.com.br>
2017-12-05 00:52:22 -0200

    Ajuste de Pastas

---
+++ dev/index.php
@@ -0,0 +1,11 @@
+
+<?php
+
+// Muestra toda la información, por defecto INFO_ALL
+phpinfo();
+
+// Muestra solamente la información de los módulos.
+// phpinfo(8) hace exactamente lo mismo.
+phpinfo(INFO_MODULES);
+
+?>
